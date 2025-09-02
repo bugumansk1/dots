ae372fce478cbbb55ecab9c109a7a72ba876be17
bugumansk1 <181235128+bugumansk1@users.noreply.github.com>
2025-09-02 05:40:53 +0000

    everforest rice

---
+++ artix/nvim/lua/plugins/everforest.lua
@@ -0,0 +1,8 @@
+return {
+  'sainnhe/everforest',
+  config = function()
+    vim.g.everforest_background = 'soft'
+    vim.g.everforest_transparent_background = 1
+    vim.cmd.colorscheme("everforest")
+  end,
+}
